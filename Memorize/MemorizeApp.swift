e15ce9aedf7643e9c6459215d43b70e1f08816ad
SeongMin-K <eo1tks3@naver.com>
2021-10-18 20:37:08 +0900

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by SeongMinK on 2021/10/18.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
